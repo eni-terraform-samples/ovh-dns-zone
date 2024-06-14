1c59964e792400a9a0f9161d98669e3f0c485f2d
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-06-14 10:10:36 +0200

    📍 : add version constraints

--- provider.tf
+++ provider.tf
@@ -1,11 +1,13 @@
 terraform {
   required_providers {
     ovh = {
-      source = "ovh/ovh"
+      source  = "ovh/ovh"
+      version = "~> 0.45.0"
     }
   }
+  required_version = ">= 1.8"
 }
 
 provider "ovh" {
-  endpoint      = "ovh-eu"
+  endpoint = "ovh-eu"
 }
