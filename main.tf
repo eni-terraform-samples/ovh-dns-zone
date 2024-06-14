8af5ae3a49ede1b7084edcb40bbcb99436146714
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-06-14 09:44:27 +0200

    ✨ : add dns_zone records

---
+++ main.tf
@@ -0,0 +1,19 @@
+data "ovh_domain_zone" "zone" {
+  name = "star-wars.ovh"
+}
+
+resource "ovh_domain_zone_record" "dagobah" {
+  zone      = data.ovh_domain_zone.zone.id
+  subdomain = "dagobah"
+  fieldtype = "A"
+  ttl       = 60
+  target    = "47.41.108.60"
+}
+
+resource "ovh_domain_zone_record" "tatooine" {
+  zone      = data.ovh_domain_zone.zone.id
+  subdomain = "tatooine"
+  fieldtype = "A"
+  ttl       = 60
+  target    = "242.105.108.11"
+}
